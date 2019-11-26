31ef5b6d16ec05452f8c60b1545fc388e62ff215
Chandon Porter <cneilporter94@gmail.com>
2019-11-26 00:21:18 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,24 +1,24 @@
-import React from 'react'
+// import React from 'react'
 
-class SimpleComponent extends Component {
+// class SimpleComponent extends Component {
   
-  constructor(props) {
-    super(props)
+//   constructor(props) {
+//     super(props)
     
-    this.state = {
-      mood: 'happy'
+//     this.state = {
+//       mood: 'happy'
       
-    }
-  }
+//     }
+//   }
   
-  handleClick = () => {
+//   handleClick = () => {
     
-  }
+//   }
   
-  render() {
+//   render() {
     
-  }
+//   }
   
-}
+// }
 
-export default SimpleComponent
+// export default SimpleComponent
